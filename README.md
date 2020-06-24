43f308edb677d5259f6a9dd7745c0fa5ae0bb742
BEST-KOMP\Admin <chertenok_le@mail.ru>
2020-06-25 01:23:56 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+CT Test
+
+Dev org setup
+Clone the project to the local machine from GIT
+Select the folder with Salesforce DX project:
+Login to your Dev Hub (don't forget to enable Dev Hub in Org Setup)
+`sfdx force:auth:web:login -d -a ct`
+
+Create a scratch org with scratch script:
+`scripts/scratch.sh dev1`
---
+++ scripts/scratch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# Dev org alias should not be empty
+if [ -z "$1" ]
+then
+    echo "You should pass scratch org alias as the first param"
+    exit 0
+fi
+
+ORG_ALIAS=$1
+ORG_NAME="CT Scratch $1"
+
+# Update SFDX Cli
+sfdx update
+
+# By default org's lifetime equals 30 days
+sfdx force:org:create orgName="$ORG_NAME" -f config/project-scratch-def.json -s -a $ORG_ALIAS -d 30 --loglevel error
+
+# Deploy metadata and settings, configure org
+sfdx force:source:push -u $ORG_ALIAS
+
+# Assign permissions
+# sfdx force:user:permset:assign -n Meetings_Permission_Set -u $ORG_ALIAS
+
+sfdx force:org:open -u $ORG_ALIAS
